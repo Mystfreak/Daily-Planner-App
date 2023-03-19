3b70130907b0476af309963e6ccaf0b859410409
Khatra Ahmed <[khatra.ahmed@gmail.com]>
2023-03-19 17:18:49 +0000

    Styling rows

--- assets/main.js
+++ assets/main.js
@@ -11,5 +11,10 @@ let eventArr = []
 
 // for loop for Time Blocks - Counter starts at 8: meaning 8am
 for(let i = 8; i <= 17; i++){
+
+    //create row to create calender
+    let hourRow = $('<div>');
+    hourRow.addClass('row');
+    timeBlockCont.append(hourBar);
     
 }
