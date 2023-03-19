9043446eed8ec8d79ba05a172c05b44e8226dfde
Khatra Ahmed <[khatra.ahmed@gmail.com]>
2023-03-19 17:00:19 +0000

    Adding clear button.

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,15 @@
 // store current time into a variable using moment
 let currentTime = moment().format("dddd, Do MMMM YYYY");
-let currentDay = $('#currentDay')
+let currentDay = $("#currentDay")
 
+// variable from HTML
+let timeBlockCont = $("#timeBlocks");
+
+//Arrays to store events
+let blockArr = []
+let eventArr = []
+
+// for loop for Time Blocks - Counter starts at 8: meaning 8am
+for(let i = 8; i <= 17; i++){
+    
+}
